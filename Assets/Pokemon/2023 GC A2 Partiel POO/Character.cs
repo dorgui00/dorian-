605e2f2d94626397ed5d3858e5e561a534bd74e5
dorgui00 <161583707+dorgui00@users.noreply.github.com>
2025-01-10 17:48:05 +0100

    premier

---
+++ Assets/Pokemon/2023 GC A2 Partiel POO/Character.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+namespace _2023_GC_A2_Partiel_POO.Level_2
+{
+    /// <summary>
+    /// Définition d'un personnage
+    /// </summary>
+    public class Character
+    {
+        /// <summary>
+        /// Stat de base, HP
+        /// </summary>
+        int _baseHealth;
+        /// <summary>
+        /// Stat de base, ATK
+        /// </summary>
+        int _baseAttack;
+        /// <summary>
+        /// Stat de base, DEF
+        /// </summary>
+        int _baseDefense;
+        /// <summary>
+        /// Stat de base, SPE
+        /// </summary>
+        int _baseSpeed;
+
+        int _currentHealth;
+        /// <summary>
+        /// Type de base
+        /// </summary>
+        TYPE _baseType;
+
+        Equipment _equipment;
+        
+
+
+
+
+        public Character(int baseHealth, int baseAttack, int baseDefense, int baseSpeed, TYPE baseType)
+        {
+            _baseHealth = baseHealth;
+            _baseAttack = baseAttack;
+            _baseDefense = baseDefense;
+            _baseSpeed = baseSpeed;
+            _baseType = baseType;
+            _currentHealth = _baseHealth;
+            CurrentHealth = _currentHealth;
+
+           
+
+
+
+        }
+
+        
+        /// <summary>
+        /// HP actuel du personnage
+        /// </summary>
+
+
+        public int CurrentHealth { get; private set ; }
+        
+        
+
+        public TYPE BaseType { get => _baseType;}
+        /// <summary>
+        /// HPMax, prendre en compte base et equipement potentiel
+        /// </summary>
+        public int MaxHealth
+        {
+            get
+            {
+                return _baseHealth;
+            }
+        }
+        
+        /// <summary>
+        /// ATK, prendre en compte base et equipement potentiel
+        /// </summary>
+        public int Attack
+        {
+            get
+            {
+                return _baseAttack ;
+            }
+        }
+        /// <summary>
+        /// DEF, prendre en compte base et equipement potentiel
+        /// </summary>
+        public int Defense
+        {
+            get
+            {
+                return _baseDefense ;
+            }
+        }
+        /// <summary>
+        /// SPE, prendre en compte base et equipement potentiel
+        /// </summary>
+        public int Speed
+        {
+            get
+            {
+                return _baseSpeed ;
+            }
+        }
+        /// <summary>
+        /// Equipement unique du personnage
+        /// </summary>
+        public Equipment CurrentEquipment { get; private set; }
+        /// <summary>
+        /// null si pas de status
+        /// </summary>
+        public StatusEffect CurrentStatus { get; private set; }
+
+        public bool IsAlive;
+
+
+        /// <summary>
+        /// Application d'un skill contre le personnage
+        /// On pourrait potentiellement avoir besoin de connaitre le personnage attaquant,
+        /// Vous pouvez adapter au besoin
+        /// </summary>
+        /// <param name="s">skill attaquant</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ReceiveAttack(Skill s)
+        {
+            if (CurrentHealth == 0) {
+                IsAlive = false;
+            }
+            else
+            {
+                IsAlive = true;
+                _currentHealth -= (s.Power - Defense);
+            }
+        }
+        /// <summary>
+        /// Equipe un objet au personnage
+        /// </summary>
+        /// <param name="newEquipment">equipement a appliquer</param>
+        /// <exception cref="ArgumentNullException">Si equipement est null</exception>
+        public void Equip(Equipment newEquipment)
+        {
+            if (newEquipment == null)
+            {
+                throw new ArgumentNullException();
+
+            } else {
+                _baseHealth += _equipment.BonusHealth;
+                _baseAttack += _equipment.BonusAttack;
+                _baseDefense += _equipment.BonusDefense;
+                _baseSpeed += _equipment.BonusSpeed;
+                CurrentEquipment = newEquipment;
+            }
+                
+            
+        }
+        /// <summary>
+        /// Desequipe l'objet en cours au personnage
+        /// </summary>
+        public void Unequip()
+        {
+            _baseHealth -= _equipment.BonusHealth;
+            _baseAttack -= _equipment.BonusAttack;
+            _baseDefense -= _equipment.BonusDefense;
+            _baseSpeed -= _equipment.BonusSpeed;
+            CurrentEquipment = null;
+        }
+    
+
+    
+    }
+    
+    }
---
+++ Assets/Pokemon/2023 GC A2 Partiel POO/Equipment.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _2023_GC_A2_Partiel_POO.Level_2
+{
+    /// <summary>
+    /// Défintion simple d'un équipement apportant des boost de stats
+    /// </summary>
+    public class Equipment
+    {
+        public int _bonusHealth;
+        public int _bonusAttack;
+        public int _bonusDefense;
+        public int _bonusSpeed;
+        public Equipment(int bonusHealth, int bonusAttack, int bonusDefense, int bonusSpeed)
+        {
+            _bonusHealth = bonusHealth;
+            _bonusAttack = bonusAttack;
+            _bonusDefense = bonusDefense;
+            _bonusSpeed = bonusSpeed;
+            BonusHealth = bonusHealth;
+            BonusAttack = bonusAttack;
+            BonusDefense = bonusDefense;
+            BonusSpeed = bonusSpeed;
+        }
+
+        
+        public int BonusHealth { get; protected set; }
+        public int BonusAttack { get; protected set; }
+        public int BonusDefense { get; protected set ; }
+        public int BonusSpeed { get; protected set ; }
+
+    }
+}
